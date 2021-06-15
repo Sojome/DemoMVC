3bae4b19eb26c7106681af3f83a19422a1628097
Aaron Jaramillo <45837346+Sojome@users.noreply.github.com>
2021-06-14 19:20:21 -0500

    Initial commit

---
+++ DemoMVC/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoMVC.Models;
+
+namespace DemoMVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            //return View();
+            Repository R = new Repository();
+            var Model = R.GetDemo();
+            return View(Model);
+        }
+    }
+}
---
+++ DemoMVC/Models/Repository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoMVC.Models
+{
+    public class Repository
+    {
+        public Demo GetDemo()
+        {
+            return new Demo { Mensaje = "hola mundo", mensajeHtml = "<h1>hola mundo</h1>" };
+        }
+    }
+}
